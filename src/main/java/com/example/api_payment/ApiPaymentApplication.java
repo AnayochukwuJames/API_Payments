7f17d935af70f458f3a13bd3087dcd002035a9d7
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-01-29 16:01:14 +0100

    first commit

--- src/main/java/com/example/api_payment/ApiPaymentApplication.java
+++ src/main/java/com/example/api_payment/ApiPaymentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiPaymentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiPaymentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPaymentApplication.class, args);
+    }
 
 }
